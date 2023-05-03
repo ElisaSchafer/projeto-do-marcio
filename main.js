060b6ffa68233fed0fa98fb9513c8b05c3782995
ElisaSchafer <elisa.meneguini@gmail.com>
2023-05-03 09:34:45 -0300

    projeto de html e css terminado

---
+++ main.js
@@ -0,0 +1,24 @@
+let conta = 0
+let pessoas = 0
+let porcentagemGorjeta = 0
+
+function receberConta() {
+    conta = Number(document.querySelector("#conta").value)
+}
+
+function receberPessoas() {
+    pessoas = Number(document.querySelector("#pessoas").value)
+}
+
+function receberPorcentagem(porcentagem) {
+    if (porcentagem === "custom") {
+        porcentagemGorjeta = Number(document.querySelector("#gorjeta").value)
+    } else {
+        porcentagemGorjeta = porcentagem
+        const button = document.querySelector("input[value='5%']")
+        button.classList.add('button-selected')
+    }
+
+    console.log(porcentagemGorjeta)
+
+}
